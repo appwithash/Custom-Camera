0c94a8bbabade3b51416ccec23629db2a9a4f3fa
ashutoshpandey28 <ashutoshpandey.developer@gmail.com>
2021-06-25 08:57:20 +0530

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by ashutosh on 25/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
